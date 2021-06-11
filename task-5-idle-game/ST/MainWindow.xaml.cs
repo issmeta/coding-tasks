a6a67530716786e94ec4eee33bdaac80034019a0
issmeta <ssmeta4@mail.ru>
2021-06-11 09:18:08 +0300

    1

---
+++ task-5-idle-game/ST/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace task_5_idle_game
+{
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        setting settingWin;
+        ulong pricePowerlevel;
+        ulong priceAutolevel;
+        ulong powerlevel;
+        ulong score;
+        ulong autolevel;
+        ulong sumclick;
+        DispatcherTimer autoTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            autoTimer = new DispatcherTimer();
+            autoTimer.Tick += new EventHandler(autoTimer_Tick);
+            autoTimer.Interval = new TimeSpan(0, 0, 1);
+            autoTimer.Start();
+            powerlevel = 1;
+            score = 0;
+            autolevel = 0;
+            sumclick = 0;
+            pricePowerlevel = 10;
+            priceAutolevel = 100;
+        }
+
+        private void autoTimer_Tick(object sender, EventArgs e)
+        {
+            score = score + autolevel;
+            labelScore.Content = Convert.ToString(score);
+
+            if (score >= 100)
+            {
+                workLabel.Background = Brushes.Green;
+            }
+            if (sumclick >= 100)
+            {
+                clackLabel.Background = Brushes.Green;
+            }
+            if (autolevel + powerlevel > 25)
+            {
+                newLabel.Background = Brushes.Green;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            settingWin = new setting();
+            settingWin.Owner = this;
+            settingWin.Show();
+        }
+
+        private void upPowerClickButtuon_Click(object sender, RoutedEventArgs e)
+        {
+            if (score >= pricePowerlevel)
+            {
+                powerlevel = powerlevel + 1;
+                score = score - pricePowerlevel;
+                pricePowerlevel = powerlevel * 10;
+
+                upPowerClickButtuon.Content = Convert.ToString(pricePowerlevel);
+                upClick.Content = "Купить силу клика - " + Convert.ToString(powerlevel - 1) + " уровня";
+            }
+        }
+
+        private void upAutoClickButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (score >= priceAutolevel)
+            {
+                autolevel = autolevel + 1;
+                score = score - priceAutolevel;
+                priceAutolevel = autolevel * 10;
+
+                upAutoClickButton.Content = Convert.ToString(priceAutolevel);
+                upAuto.Content = "Купить автоклик - " + Convert.ToString(autolevel) + " уровня";
+            }
+        }
+
+        private void ClacButton1_Click(object sender, RoutedEventArgs e)
+        {
+            score = score + powerlevel;
+            sumclick = sumclick + 1;
+            labelScore.Content = Convert.ToString(score);
+        }
+    }
+}
---
+++ task-5-idle-game/ST/setting.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace task_5_idle_game
+{
+    /// <summary>
+    /// Логика взаимодействия для setting.xaml
+    /// </summary>
+    public partial class setting : Window
+    {
+        public setting()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)this.Owner;
+            mainWindow.upClick.FontFamily = but1.FontFamily;
+            mainWindow.upPowerClickButtuon.FontFamily = but1.FontFamily;
+            mainWindow.upAuto.FontFamily = but1.FontFamily;
+            mainWindow.upAutoClickButton.FontFamily = but1.FontFamily;
+            mainWindow.labelScore.FontFamily = but1.FontFamily;
+            mainWindow.check.FontFamily = but1.FontFamily;
+            mainWindow.workLabel.FontFamily = but1.FontFamily;
+            mainWindow.clackLabel.FontFamily = but1.FontFamily;
+            mainWindow.newLabel.FontFamily = but1.FontFamily;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)this.Owner;
+            mainWindow.upClick.FontFamily = but2.FontFamily;
+            mainWindow.upPowerClickButtuon.FontFamily = but2.FontFamily;
+            mainWindow.upAuto.FontFamily = but2.FontFamily;
+            mainWindow.upAutoClickButton.FontFamily = but2.FontFamily;
+            mainWindow.labelScore.FontFamily = but2.FontFamily;
+            mainWindow.check.FontFamily = but2.FontFamily;
+            mainWindow.workLabel.FontFamily = but2.FontFamily;
+            mainWindow.clackLabel.FontFamily = but2.FontFamily;
+            mainWindow.newLabel.FontFamily = but2.FontFamily;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)this.Owner;
+            mainWindow.upClick.FontFamily = but3.FontFamily;
+            mainWindow.upPowerClickButtuon.FontFamily = but3.FontFamily;
+            mainWindow.upAuto.FontFamily = but3.FontFamily;
+            mainWindow.upAutoClickButton.FontFamily = but3.FontFamily;
+            mainWindow.labelScore.FontFamily = but3.FontFamily;
+            mainWindow.check.FontFamily = but3.FontFamily;
+            mainWindow.workLabel.FontFamily = but3.FontFamily;
+            mainWindow.clackLabel.FontFamily = but3.FontFamily;
+            mainWindow.newLabel.FontFamily = but3.FontFamily;
+        }
+    }
+}
